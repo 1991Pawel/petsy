e6f8ab41f7f1ceac26f257ae9d0a4d68a4dcc4b7
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-05-23 22:54:53 +0200

    add HotelCard and HotelList

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import { Container } from "./components/Container/Container";
+import { HotelList } from "./components/HotelList/HotelList";
 
 export default function Home() {
-    return <div className="text-rose-500 text-2xl">Logo</div>;
+    return (
+        <div className="pt-[100px]">
+            <Container>
+                <HotelList />
+            </Container>
+        </div>
+    );
 }
